a299126acd23582a20bdba7e5fc3b15358666f0d
MishynaIra <gortenzia1987@gmail.com>
2022-11-15 10:13:10 +0200

    fixed logika

--- src/index.js
+++ src/index.js
@@ -13,48 +13,66 @@ const refs = {
 
  refs.searchBox.addEventListener('input', debounce(onSearchinput), DEBOUNCE_DELAY);
 
-function onSearchinput (e) {
-   e.preventDefault();
-   const name = e.target.value.trim();//собираем с формы значение
+ function onSearchinput() {
+   const name = refs.searchBox.value.trim()
+   if (name === '') {
+     return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '')
+   }//собираем с формы значение
 
 fetchUser(name)
-.then(showCountries) //доступ к данным
+.then(countries => {
+   refs.countryList.innerHTML = ''
+   refs.countryInfo.innerHTML = ''
+   if (countries.length === 1) {
+      refs.countryList.insertAdjacentHTML('beforeend', showCountryList(countries))
+      refs.countryInfo.insertAdjacentHTML('beforeend', showCountryInfo(countries))
+   } else if (countries.length >= 10) {
+      showMatches()
+   } else {
+     refs.countryList.insertAdjacentHTML('beforeend', showCountryList(countries))
+   }
+ })
+  
 .catch(showError);
 }
 
-function showError(error) {
-   console.log(error);
-   refs.searchBox.innerHTML = Notify.failure("Oops, there is no country with that name")
+function showError() {
+   Notify.failure("Oops, there is no country with that name")
 }
 
-function showCountries(data) {
-   console.log(data);
+function showMatches() {
+   Notify.info('Too many matches found. Please enter a more specific name.')
+ }
 
-   const element = 
-   `
-   <li class="country-list__item">
-      <img class="country-list__flag" src="${data[0].flags.svg}" alt="Flag of ${data[0].name.official}" width = 50px height = 50px>
-      <p class="country-list__name">${data[0].name.official}</p>
-   </li>
-
-   `
-
-   const country = 
-   `
-   <li class="country-list__item">
-      <img class="country-list__flag" src="${data[0].flags.svg}" alt="Flag of ${data[0].name.official}" width = 50px height = 50px>
-      <p class="country-list__name">${data[0].name.official}</p>
-   </li>
-   <li class="country-list__item">${data[0].capital }</li>
-   <li class="country-list__item">${data[0].population}</li>
-   <li class="country-list__item">${data[0].languages}</li>
-
-   `
-   console.log(data[0].languages);
-   refs.countryList.innerHTML = element;
-   refs.countryInfo.innerHTML = country;
+function showCountryList(countries) {
+   const markup = countries
+   .map(({name, flags}) => {
+      return `
+      <li class="country-list__item">
+         <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 50px height = 50px>
+         <p class="country-list__name">${name.official}</p>
+      </li>
+      `
+   }).join('')
+   return markup
 }
+   
 
+   function showCountryInfo(countries) {
+      const markup = countries
+   .map(({ capital, population, languages }) => {
+      return `
+      <ul class="country-info__list">
+      <li class="country-list__item"><p><b>Capital: </b>${capital}</p></li>
+      <li class="country-list__item"><p><b>Population: </b>${population}</p></li>
+      <li class="country-list__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+      </ul>
+      `
+   }).join('')
+   return markup
+}
+   
+ 
 
 
 
